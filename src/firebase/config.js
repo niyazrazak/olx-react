96b33145cb41e74611258305e70fe840aea21996
Niyaz Razak <niyasibnurazak@gmail.com>
2023-08-17 22:47:06 +0300

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDH8wJ96YR_gf1jEGar655WIOajHUmplNg",
+    authDomain: "ol-clone.firebaseapp.com",
+    projectId: "ol-clone",
+    storageBucket: "ol-clone.appspot.com",
+    messagingSenderId: "1068926135781",
+    appId: "1:1068926135781:web:ac2a513e4040dafa1d530c",
+    measurementId: "G-X3KFCE75YJ"
+};
+
+
+export default firebase.initializeApp(firebaseConfig)
